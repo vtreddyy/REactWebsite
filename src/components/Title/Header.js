ca7decb59a19e0c4c82dad3db71577bcf245865c
Tarun Vuyyuru <Tarunvuyyur@gmail.com>
2023-08-16 14:31:48 -0400

    added sepearate component for the login button

--- src/components/Title/Header.js
+++ src/components/Title/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Logo from './Logo';
 import TitleName from './TitleName';
+import LoginButton from './LoginButton';
 import './Header.css'; 
 const Header = () => {
     return (
-    <header className="header">
-      <div className="header__container">
-        <Logo />
-        <TitleName/>
-        <button className="btn btn-outline header__button" type="button">Log In</button>
-      </div>
-    </header>
-  );
+        <header className="header">
+            <div className="header__container">
+                <Logo />
+                <TitleName/>
+                <LoginButton /> 
+            </div>
+        </header>
+    );
 };
 
 export default Header;
---
+++ src/components/Title/LoginButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LoginButton.css';
+
+const LoginButton = () => {
+    return (
+        <button className="btn btn-outline header__button" type="button">Log In</button>
+    );
+};
+
+export default LoginButton;
